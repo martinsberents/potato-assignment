670068e5862dce2556e93a572939f013f030d6ad
Martins Berents <martins.berents@gmail.com>
2016-04-04 22:24:18 +0300

    Crispy >= 1.5.x requires CRISPY_ALLOWED_TEMPLATE_PACKS setting to be defined

--- tracker/settings.py
+++ tracker/settings.py
@@ -137,6 +137,7 @@ if DEBUG:
     CSP_STYLE_SRC = ("'self'", "'unsafe-inline'")
     CSP_IMG_SRC += ("*.placehold.it",)
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = ('bootstrap', 'uni_form', 'bootstrap3', 'foundation-5')
 CRISPY_TEMPLATE_PACK = 'foundation-5'
 
 AUTH_USER_MODEL = 'djangae.GaeUser'
